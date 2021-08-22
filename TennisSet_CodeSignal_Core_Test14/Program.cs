52061a0d3a11ade447cb6e8df949d83040fb6435
Albert Hakobyan <abhakobyan24@gmail.com>
2021-08-22 18:12:58 +0400

    Добавьте файлы проекта.

---
+++ TennisSet_CodeSignal_Core_Test14/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisSet_CodeSignal_Core_Test14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
