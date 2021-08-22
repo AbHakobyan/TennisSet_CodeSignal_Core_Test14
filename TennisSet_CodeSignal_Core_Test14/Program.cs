3544f00768a2e1cf0b366d2e646304ea6a83bc4f
Albert Hakobyan <abhakobyan24@gmail.com>
2021-08-22 18:24:58 +0400

    Add Method tennisSet

--- TennisSet_CodeSignal_Core_Test14/Program.cs
+++ TennisSet_CodeSignal_Core_Test14/Program.cs
@@ -10,6 +10,43 @@ namespace TennisSet_CodeSignal_Core_Test14
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Input Score1");
+            int score1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input Score2");
+            int score2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(tennisSet(score1,score2));
+        }
+        static bool tennisSet(int score1, int score2)
+        {
+            if (score1 == 6 && score1 < 7 && score2 < 5)
+            {
+                return true;
+            }
+            else if (score2 == 6 && score2 < 7 && score1 < 5)
+            {
+                return true;
+            }
+            else if (score1 == 7 && score1 < 8 && score2 < 5)
+            {
+                return false;
+            }
+            else if (score2 == 7 && score2 < 8 && score1 < 5)
+            {
+                return false;
+            }
+            else if (score1 == 7 && score1 < 8 && score2 <= 6)
+            {
+                return true;
+            }
+            else if (score2 == 7 && score2 < 8 && score1 <= 6)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            return false;
         }
     }
 }
